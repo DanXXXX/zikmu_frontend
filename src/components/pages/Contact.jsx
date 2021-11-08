feb7ea0915ed3e385efff4e862cbe5e51b0dfe8f
Dev-Ninja75 <sofiane.ab@gmail.com>
2021-11-08 14:28:11 +0100

    Import and set up contact route

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Contact() {
   return (
     <div className="contactForm">
-      <h1 className="title"> Contact</h1>
+      <h1 className="title">Contact</h1>
     </div>
   );
 }
