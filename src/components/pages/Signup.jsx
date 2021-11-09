447b87f95f1f927bdd23ecd5e0f8846ee36613aa
Dev-Ninja75 <sofiane.ab@gmail.com>
2021-11-09 16:43:19 +0100

    Add Signup route to App.js and Navbar

--- src/components/pages/Signup.jsx
+++ src/components/pages/Signup.jsx
@@ -1,10 +1,10 @@
-import react from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 
 export default function SignUp() {
   return (
     <div>
-      <h1 className="title">SignUP</h1>
+      <h1 className="title">Sign Up</h1>
     </div>
   );
 }
