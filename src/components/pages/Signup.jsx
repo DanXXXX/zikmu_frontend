54e1f878e0d0a94098de2ed116f74c148bd900c2
Dev-Ninja75 <sofiane.ab@gmail.com>
2021-11-09 16:32:45 +0100

    Setup Signup component

---
+++ src/components/pages/Signup.jsx
@@ -0,0 +1,10 @@
+import react from "react";
+import { useForm } from "react-hook-form";
+
+export default function SignUp() {
+  return (
+    <div>
+      <h1 className="title">SignUP</h1>
+    </div>
+  );
+}
