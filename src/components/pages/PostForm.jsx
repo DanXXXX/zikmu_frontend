b718135104599b74204f389e62efcbacc26ec4ff
Dev-Ninja75 <sofiane.ab@gmail.com>
2021-11-10 12:00:24 +0100

    Setup PostForm component

---
+++ src/components/pages/PostForm.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import axios from "axios";
+
+export default function PostForm() {
+  return (
+    <div>
+      <h1 className="title">Rédiger un post</h1>
+    </div>
+  );
+}
