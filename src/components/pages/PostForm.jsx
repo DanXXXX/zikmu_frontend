d4d491793ec1e0c98bb8a6c7ddca8edbd0f2ecc5
Dev-Ninja75 <sofiane.ab@gmail.com>
2021-11-10 12:28:29 +0100

    add and setup axios

--- src/components/pages/PostForm.jsx
+++ src/components/pages/PostForm.jsx
@@ -1,7 +1,6 @@
 import react from "react";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
-import axios from "axios";
 
 export default function PostForm() {
   const {
