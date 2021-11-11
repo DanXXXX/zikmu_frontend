40ec6a0c5b279540dcc32bef08e5af00896b9c95
Dev-Ninja75 <sofiane.ab@gmail.com>
2021-11-11 08:01:58 +0100

    Add useForm and useState

--- src/components/pages/CourseForm.jsx
+++ src/components/pages/CourseForm.jsx
@@ -1,6 +1,17 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 export default function CourseForm() {
+  const {
+    formState: { errors },
+  } = useForm();
+
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+  const [file, setFile] = useState("");
+  const [price, setPrice] = useState("");
+
   return (
     <div>
       <h1 className="title">Proposer un cours</h1>
