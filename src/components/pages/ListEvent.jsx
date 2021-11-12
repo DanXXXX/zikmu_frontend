cd4fd50f960517a8c16179b4689438c272779a1a
Dev-Ninja75 <sofiane.ab@gmail.com>
2021-11-12 13:39:36 +0100

    cleaning ListEvent

--- src/components/pages/ListEvent.jsx
+++ src/components/pages/ListEvent.jsx
@@ -1,51 +1,35 @@
-import React, { useState, useEffect } from 'react'
-import Axios from "axios"
-import axios from 'axios';
-import Event from '../Event';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Event from "../Event";
 
 export default function ListEvent() {
+  const baseUrl = "http://localhost:4000";
+  const [data, setData] = useState([]);
 
-    const baseUrl = "http://localhost:4000"
-    const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get(`${baseUrl}/event`).then((res) => {
+      console.log(res.data);
+      setData(res.data.events);
+    });
+  }, []);
 
-
-
-    useEffect(() => {
-        axios.get(`${baseUrl}/event`).then((res) => {
-            console.log(res.data);
-            setData(res.data.events)
-        })
-
-    }, []);
-
-    // function createPost() {
-    //     axios
-    //         .post(baseUrl, {
-    //             title: "Hello",
-    //             body: "body to send"
-    //         })
-    //         .then((res) => {
-    //             setData(res.data)
-    //         })
-    // }
-
-    return (
-        <div id="event">
-
-            <ul>
-                {data?.map((docs) => {
-                    console.log(docs.title);
-                    return (
-                        <>
-                            <Event image={docs.file} title={docs.title}
-                                category={docs.category} location={docs.location} />
-                        </>
-                    )
-                }
-
-                )}
-            </ul>
-
-        </div>
-    )
+  return (
+    <div id="event">
+      <ul>
+        {data?.map((docs) => {
+          console.log(docs.title);
+          return (
+            <>
+              <Event
+                image={docs.file}
+                title={docs.title}
+                category={docs.category}
+                location={docs.location}
+              />
+            </>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
