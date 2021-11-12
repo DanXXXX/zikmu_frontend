f41e7a0c593ead3afbe211a015279a8d2a0084a1
Dev-Ninja75 <sofiane.ab@gmail.com>
2021-11-12 05:30:42 +0100

    Setup axios

--- src/components/pages/EventForm.jsx
+++ src/components/pages/EventForm.jsx
@@ -1,43 +1,57 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
+import axios from "axios";
 
 export default function EventForm() {
-    const {
-        formState: {errors}, 
-    } = useForm();
+  const {
+    formState: { errors },
+  } = useForm();
 
-    const [title, setTitle] = useState("");
-    const [text, setText] = useState("");
-    const [category, setCategory] = useState("");
-    const [file, setFile] = useState(""); 
-    const [location, setLocation] = useState(""), 
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState("");
+  const [file, setFile] = useState("");
+  const [location, setLocation] = useState("");
 
-    const handleTitle = (e) => {
-        setTitle(e.target.value);
-    };
+  const handleTitle = (e) => {
+    setTitle(e.target.value);
+  };
 
-    const handleText = (e) => {
-        setText(e.target.value);
-    };
+  const handleText = (e) => {
+    setText(e.target.value);
+  };
 
-    const handleCategory = (e) => {
-        setCategory(e.target.value)
-    };
+  const handleCategory = (e) => {
+    setCategory(e.target.value);
+  };
 
-    const handleFile = (e) => {
-        setFile(e.target.value)
-    };
+  const handleFile = (e) => {
+    setFile(e.target.value);
+  };
 
-    const handleLocation = (e) => {
-        setLocation(e.target.value)
-    };
+  const handleLocation = (e) => {
+    setLocation(e.target.value);
+  };
 
-    return (
-        <div>
-            <h1 className="title">Ajouter un évènement</h1>
-        </div>
+  const requestOptions = () => {
+    axios({
+      method: "POST",
+      url: "http://localhost:4000/event/submit",
+      data: { title, text, category, file, location },
+    }).then((response) => {
+      if (response.data.status === "success") {
+        alert("Event sent.");
+        this.restForm();
+      } else if (response.data.status === "fail") {
+        alert("Event failed to send. ");
+      }
+    });
+  };
 
-    );
-    
-};
+  return (
+    <div>
+      <h1 className="title">Ajouter un évènement</h1>
+    </div>
+  );
+}
