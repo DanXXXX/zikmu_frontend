76f21e3884970ccb1f6d6bddd89c49877dc3e50b
Dev-Ninja75 <sofiane.ab@gmail.com>
2021-11-13 08:14:26 +0100

    Add Link to user page

--- src/components/pages/UserProfile.jsx
+++ src/components/pages/UserProfile.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function UserProfile(props) {
   const [user, setUser] = useState([]);
@@ -9,7 +10,7 @@ export default function UserProfile(props) {
 
   useEffect(() => {
     fetch(apiUrl + "/users/" + id)
-      .then(res.json())
+      .then((res) => res.json())
       .then((data) => setUser(data));
   }, []);
 
@@ -32,13 +33,22 @@ export default function UserProfile(props) {
         <h1>Profil</h1>
         <div className="userDashboard">
           <div className="avatar">
-            <img src={apiUrl + image + _id} alt="profile picture" />
+            <img
+              src={apiUrl + image + _id}
+              alt={`${fullName}profile picture`}
+            />
           </div>
           <div className="fullName">
             <h2>{fullName}</h2>
           </div>
           <div className="surname">
             <h3>{surname}</h3>
+          </div>
+          <div>
+            <Link to="/messages">{messages}</Link>
+          </div>
+          <div>
+            <Link to="/likes">{likes}</Link>
           </div>
           <div className="posts">
             <p>{posts}</p>
