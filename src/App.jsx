928de2e72daf101ad971228d9f22f259c9467bc2
AlexandreC93 <alexandrecadinot93@gmail.com>
2021-11-15 12:00:13 +0100

    cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 //import { UserContext } from "../App.js";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
-import Home from "./components/pages/Home";
-import Update from "./components/pages/admin/Update";
-import Register from "./components/pages/Register";
-import SignUp from "./components/pages/Signup";
-import Login from "./components/pages/Login";
-import Contact from "./components/pages/Contact";
-import PostForm from "./components/pages/PostForm";
-import EventForm from "./components/pages/EventForm";
-import MusicForm from "./components/pages/MusicForm";
-import Error404 from "./components/pages/Error404";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js.map";
 import "./styles/main.scss";
-import ListEvent from "./components/pages/ListEvent";
-import Actu from "./components/pages/Actu";
-import Map from "./components/pages/Map";
-import CourseForm from "./components/pages/CourseForm";
-import UserProfile from "./components/pages/UserProfile";
+import Routes from "./components/Routes";
 
 function App() {
   const [id, setId] = useState({});
@@ -30,26 +15,9 @@ function App() {
 
   return (
     <div>
-      <BrowserRouter>
-        <Header />
-
-        <Switch>
-          <Route exact path="/" component={Actu} />
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={SignUp} />
-          <Route path="/contact" component={Contact} />
-          <Route exact path="/event" component={ListEvent} />
-          <Route path="/event/map" component={Map} />
-          <Route path="/event/submit" component={EventForm} />
-          <Route path="/post/submit" component={PostForm} />
-          <Route path="/course/submit" component={CourseForm} />
-          <Route path="/songs/submit" component={MusicForm} />
-          <Route path="/user/profil" component={UserProfile} />
-          <Route path="*" component={Error404} />
-        </Switch>
-
-        <Footer />
-      </BrowserRouter>
+      <Header />
+      <Routes />
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import rootReducer from './reducers'
+
+const store = createStore(
+  rootReducer, composeWithDevTools(applyMiddleware(thunk, logger))
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  < Provider store={store} >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  </Provider >,
+  document.getElementById('root')
+)
 reportWebVitals();
+
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
