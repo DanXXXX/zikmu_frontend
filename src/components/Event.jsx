86b692b813abf9e8ba5c053f987dfe4e365ff63c
AlexandreC93 <alexandrecadinot93@gmail.com>
2021-11-15 10:02:52 +0100

    map done

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React from "react";
 
-export default function Event({image, title, text, category, location}) {
-    return (
-        <div id='event'>
-            <img src={image} alt="" />
-            <h3>{title}</h3>
-            <p>{text}</p>
-            <h5>{category}</h5>
-            <p>{location}</p>
-        </div>
-    )
+export default function Event({
+  image,
+  title,
+  text,
+  category,
+  location,
+  coordinates,
+}) {
+  console.log(coordinates);
+  return (
+    <div id="event">
+      <img src={image} alt="" />
+      <h3>{title}</h3>
+      <p>{text}</p>
+      <h5>{category}</h5>
+      <p>{location}</p>
+      <p>
+        {coordinates?.lat}
+        {coordinates?.lng}
+      </p>
+    </div>
+  );
 }
--- src/components/pages/Map.jsx
+++ src/components/pages/Map.jsx
@@ -3,42 +3,49 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import axios from "axios";
 import ReactModal from "react-modal";
 
-export default function LocationMarker() {
+export default function LocationMarker({
+  image,
+  title,
+  category,
+  location,
+  coordinates,
+}) {
   const [locations, setLocations] = useState([]);
   useEffect(() => {
     axios
-      .get(`http://localhost:4000/event/`)
+      .get(`http://localhost:4000/event`)
       .then((res) => {
-        setLocations(res.data.coordinates);
-        console.log(res.data.coordinates);
+        setLocations(res.data.events);
+        console.log(res.data.events);
       })
       .catch((err) => console.log(err));
   }, []);
   const LocationMarker = () => {
     console.log("marker");
-    return locations === []
-      ? null
-      : locations?.map((location) => (
-          <Marker
-            position={[
-              location.coordinates.lat || 40.0,
-              location.coordinates.lng || 2.0,
-            ]}
-          >
-            <Popup className="pop-up">
-              <h3>{location.title}</h3>
-              <p>{location.address}</p>
-              <button>Details</button>
-            </Popup>
-          </Marker>
-        ));
+    return locations?.map((location) => {
+      console.log(location);
+      return (
+        <Marker
+          position={[
+            location.coordinatesObject.lat || 40.0,
+            location.coordinatesObject.lng || 2.0,
+          ]}
+        >
+          <Popup className="pop-up">
+            <h3>{location.title}</h3>
+            <p>{location.text}</p>
+            <button>Details</button>
+          </Popup>
+        </Marker>
+      );
+    });
   };
   return (
     <div className="map">
       <MapContainer
         className="map-container"
         center={[46.8450326, 2.3997593]}
-        zoom={5}
+        zoom={6}
         scrollWheelZoom={true}
       >
         <TileLayer
