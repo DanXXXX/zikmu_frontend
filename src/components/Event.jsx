52d7d06bb4a2d570b0de554ba3303115c5446f25
Dev-Ninja75 <sofiane.ab@gmail.com>
2021-11-16 17:32:19 +0100

    Stylin Event test

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -10,12 +10,14 @@ export default function Event({
 }) {
   console.log(coordinates);
   return (
-    <div id="event">
-      <img src={image} alt="" />
-      <h3>{title}</h3>
-      <p>{text}</p>
-      <h5>{category}</h5>
-      <p>{location}</p>
+    <div className="eventContainer" id="event">
+      <div className="divider">
+        <img src={image} alt="" />
+        <h3>{title}</h3>
+        <p>{text}</p>
+        <h5>{category}</h5>
+        <p>{location}</p>
+      </div>
     </div>
   );
 }
